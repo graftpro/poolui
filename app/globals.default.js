6cd64711922ebc80a5663ef41ed5ba92e2842eb8
nikashitsa <vernik91@gmail.com>
2018-02-20 19:24:05 +0600

    Update config

--- app/globals.default.js
+++ app/globals.default.js
@@ -4,8 +4,8 @@ angular.module('pool.globals', [])
 
 .factory('GLOBALS', function() {
 	return {
-		pool_name: "GRAFTpool.online",
-		api_url : 'https://api2.graftpool.online/api',
+		pool_name: "GraftPro.com",
+		api_url : 'https://api.graftpro.com/api',
 		api_refresh_interval: 5000,
 		app_update_interval: 30*60000
 	};
